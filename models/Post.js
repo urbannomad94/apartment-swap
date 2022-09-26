1d744952134fa45dffcf460b4a88479f32526f5f
Aidan Williams <aidanw4@gmail.com>
2022-09-26 10:43:26 +0700

    first commit

---
+++ models/Post.js
@@ -0,0 +1,55 @@
+const mongoose = require('mongoose');
+const moment = require('moment');
+
+const PostSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  image: {
+    type: String,
+    require: true,
+  },
+  cloudinaryId: {
+    type: String,
+    require: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  country: {
+    type: String,
+    required: true,
+  },
+  city: {
+    type: String,
+    required: true,
+  },
+  petFriendly: {
+    type: String,
+    required: true,
+  },
+  startDate: {
+    type: Date,
+    required: false,
+  },
+  endDate: {
+    type: Date,
+    required: false,
+  },
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+  likes: {
+    type: Number,
+    default: 0,
+  },
+});
+
+module.exports = mongoose.model('Post', PostSchema);
